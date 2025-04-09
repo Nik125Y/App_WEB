4f2a27ed84ac56a2fe7e27feb256baf6cbafb1f1
125Y <topnikm@gmail.com>
2025-04-09 20:49:56 +0300

    Update form.php

--- work6/form.php
+++ work6/form.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <link rel="stylesheet" href="style.css" />
   <link rel="stylesheet" href="bootstrap.min.css" />
-  <title>Zadacha 6</title>
+  <title>Задание_6</title>
 </head>
 
 <body>
