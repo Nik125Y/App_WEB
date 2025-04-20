dfcb578c372ae2c1ade333d9bfb7507f40801924
125Y <topnikm@gmail.com>
2025-04-20 22:35:26 +0300

    Update and rename admin(1).php to admin.php

--- work7/admin.php
+++ work7/admin.php
@@ -53,7 +53,7 @@ if (count($_POST)) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="bootstrap.min.css">
   <link rel="stylesheet" href="admin.css">
-  <title>Задание_6_админка</title>
+  <title>Задание_7_админка</title>
 </head>
 
 <body>
