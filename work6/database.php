e56c99470854000f05bdb18da133670f2a84aebd
125Y <topnikm@gmail.com>
2025-04-20 22:54:20 +0300

    Delete work6/database.php

--- work6/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$user = 'u68791'; 
-	$pass = '1609462'; 
-	$db = new PDO('mysql:host=localhost;dbname=u68791', $user, $pass,
-	[PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
-?>
